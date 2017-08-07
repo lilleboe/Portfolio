e63260d7265b9fee83a3fdad67714aa364471d45
slilleboe3 <scott.lilleboe@gatech.edu>
2017-08-07 07:59:30 -0700

    initial files

---
+++ js/mypopup.js
@@ -0,0 +1,7 @@
+var e = document.getElementById('parent');
+e.onmouseover = function() {
+  document.getElementById('popup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('popup').style.display = 'none';
+}
